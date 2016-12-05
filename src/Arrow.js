15541709cb0199d7ee5f0ff290b59c37115fb0f4
Ryan Williams <ryan.blake.williams@gmail.com>
2016-12-05 21:22:54 +0000

    rm some comments

--- src/Arrow.js
+++ src/Arrow.js
@@ -3,17 +3,9 @@ import React, { Component } from 'react';
 
 import { M, L } from './path';
 
-// const { sqrt, min } = Math;
-
 export default class Arrow extends Component {
   render() {
     let { fromX, fromY, toX, toY, ...rest } = this.props;
-    // const dx = toX - fromX, dy = toY - fromY;
-    // const len = sqrt(dx*dx + dy*dy);
-    // head = head || min(10, len);
-    // const r = head / len * sqrt(3) / 2;
-    // const r2 = head / len / 2;
-    // const mx = toX + (fromX - toX) * r, my = toY + (fromY - toY) * r;
     return <path
           key="stem"
           className="arrow"
